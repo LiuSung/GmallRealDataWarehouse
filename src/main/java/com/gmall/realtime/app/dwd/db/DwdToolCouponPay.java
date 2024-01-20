ef886c077efc02d8da4c6f3bd93cd52ab43cbbfe
xuan.liu <admirefx@163.com>
2024-01-20 21:27:30 +0800

    工具域优惠券使用(支付)事务事实表

---
+++ src/main/java/com/gmall/realtime/app/dwd/db/DwdToolCouponPay.java
@@ -0,0 +1,75 @@
+package com.gmall.realtime.app.dwd.db;
+
+
+import com.gmall.realtime.utils.KafkaUtil;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class DwdToolCouponPay {
+    public static void main(String[] args) throws Exception {
+
+        // TODO 1. 环境准备
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+//        //1. 开启CheckPoint
+//        env.enableCheckpointing(5 * 60000L, CheckpointingMode.EXACTLY_ONCE);
+//        env.getCheckpointConfig().setCheckpointTimeout(10 * 60000L);
+//        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+//        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 5000L));
+//
+//        //2. 设置状态后端
+//        env.setStateBackend(new HashMapStateBackend());
+//        env.getCheckpointConfig().setCheckpointStorage("hdfs://192.168.141.100:9820/flink/ck");
+//        System.setProperty("HADOOP_USER_NAME", "root");
+
+
+        // TODO 3. 从 Kafka 读取业务数据，封装为 Flink SQL 表
+        tableEnv.executeSql(KafkaUtil.getTopicDb("dwdtoolcouponpay"));
+
+        // TODO 4. 读取优惠券领用表数据，筛选优惠券使用（支付）数据
+        Table couponUsePay = tableEnv.sqlQuery("select " +
+                "data['id'] id, " +
+                "data['coupon_id'] coupon_id, " +
+                "data['user_id'] user_id, " +
+                "data['order_id'] order_id, " +
+                "date_format(data['used_time'],'yyyy-MM-dd') date_id, " +
+                "data['used_time'] used_time, " +
+                "`old` " +
+                "from topic_db " +
+                "where `table` = 'coupon_use' " +
+                "and `type` = 'update' " +
+                "and data['used_time'] is not null");
+
+        tableEnv.createTemporaryView("coupon_use_pay", couponUsePay);
+
+        // TODO 5. 建立 Kafka-Connector dwd_tool_coupon_order 表
+        tableEnv.executeSql("create table dwd_tool_coupon_pay( " +
+                "id string, " +
+                "coupon_id string, " +
+                "user_id string, " +
+                "order_id string, " +
+                "date_id string, " +
+                "payment_time string " +
+                ")" + KafkaUtil.getKafkaSinkDDL("dwd_tool_coupon_pay"));
+
+        // TODO 6. 将数据写入 Kafka-Connector 表
+        tableEnv.executeSql("" +
+                "insert into dwd_tool_coupon_pay select " +
+                "id, " +
+                "coupon_id, " +
+                "user_id, " +
+                "order_id, " +
+                "date_id, " +
+                "used_time payment_time " +
+                "from coupon_use_pay").print();
+        env.execute("DwdToolCouponPay");
+    }
+}
